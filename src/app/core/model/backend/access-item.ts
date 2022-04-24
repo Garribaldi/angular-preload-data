7a87482432f550436980476a6d927b376fb41e6f
A. Grieger <a.grieger@arcor.de>
2022-04-24 14:18:50 +0200

    optimize page data update; add not found component

--- src/app/core/model/backend/access-item.ts
+++ src/app/core/model/backend/access-item.ts
@@ -1,4 +1,6 @@
 export interface AccessItem {
     id: string;
     name: string;
+    url: string;
+    destination: string | null
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,8 +3,21 @@ import { NavigationEnd, Router } from '@angular/router';
 import { AccessItem } from 'src/app/core/model/backend/access-item';
 import { AccessService } from 'src/app/core/services/backend/access.service';
 import { SharedDataService } from 'src/app/core/services/shared-data.service';
-import { filter, first, takeUntil } from 'rxjs/operators';
-import { firstValueFrom, Subject } from 'rxjs';
+import {
+  filter,
+  finalize,
+  first,
+  switchMap,
+  takeUntil,
+  tap,
+} from 'rxjs/operators';
+import {
+  BehaviorSubject,
+  combineLatest,
+  firstValueFrom,
+  of,
+  Subject,
+} from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -12,7 +25,7 @@ import { firstValueFrom, Subject } from 'rxjs';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  currentPages: AccessItem[] | undefined;
+  currentPage$ = new BehaviorSubject<AccessItem | undefined>(undefined);
 
   private unsubscribe = new Subject<boolean>();
 
@@ -31,7 +44,6 @@ export class HomeComponent implements OnInit, OnDestroy {
         takeUntil(this.unsubscribe)
       )
       .subscribe((e) => {
-        console.log(e);
         this.refresh();
       });
   }
@@ -42,18 +54,46 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   async refresh() {
-    const storedPageData = await firstValueFrom(this.sharedData.pageData$);
-    this.updataPageData(storedPageData);
+/*     this.sharedData.pageData$
+      .pipe(
+        takeUntil(this.unsubscribe),
+        first(),
+        tap((storedPages) => this.updataPageData(storedPages)),
+        switchMap((storedPages) =>
+          combineLatest([
+            of(storedPages),
+            this.accessService.getUserData().pipe(takeUntil(this.unsubscribe)),
+          ])
+        ),
+        finalize(() => {
+          if (!this.currentPage$.value) {
+            this.currentPage$.next({} as AccessItem);
+          }
+        })
+      )
+      .subscribe(([storedPages, userPages]) => {
+        if (JSON.stringify(storedPages) !== JSON.stringify(userPages)) {
+          this.sharedData.setPageData(userPages);
+          this.updataPageData(userPages);
+        }
+      }); */
 
-    const userPageData = await firstValueFrom(this.accessService.getUserData());
-    if (JSON.stringify(storedPageData) !== JSON.stringify(userPageData)) {
-      this.sharedData.setPageData(userPageData);
-      this.updataPageData(userPageData);
+    const storedPages = await firstValueFrom(this.sharedData.pageData$);
+    this.updataPageData(storedPages);
+
+    const userPages = await firstValueFrom(this.accessService.getUserData());
+    if (JSON.stringify(storedPages) !== JSON.stringify(userPages)) {
+      this.sharedData.setPageData(userPages);
+      this.updataPageData(userPages);
+    }
+
+    if (!this.currentPage$.value) {
+      this.currentPage$.next({} as AccessItem);
     }
   }
 
   updataPageData(pages: AccessItem[]): void {
-    console.log('update');
-    this.currentPages = pages;
+    const currentUrl = this.router.url.replace(/^\//, '');
+    this.currentPage$.next(pages.find((pages) => pages.url === currentUrl));
   }
 }
